2cc2edf1d9164f547022223fb385ca83b31ee36c
gustavo candido da silva <gustavo_gcs@outlook.com>
2021-04-18 18:04:46 -0300

    montagem da estrutura

--- src/app/components/file-viewer/models/button.ts
+++ src/app/components/file-viewer/models/button.ts
@@ -4,13 +4,15 @@ export class Button {
     public Texto:string;
     public OnClick:string;
     public ButtonStyle:string;
+    public ButtonClass:string;
 
-    constructor(_HTML_id:string, _Texto:string, _OnClick:string, _ButtonStyle:string) {
+    constructor(_HTML_id:string, _Texto:string, _OnClick:string, _ButtonStyle:string, _ButtonClass:string) {
 
         this.HTML_id = _HTML_id;
         this.Texto = _Texto;
         this.OnClick = _OnClick;
         this.ButtonStyle = _ButtonStyle;
+        this.ButtonClass = _ButtonClass;
 
     }
 
