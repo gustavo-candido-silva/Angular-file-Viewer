f526a35620c5b5c543a88571d5062fa70c6440de
gustavo candido da silva <gustavo_gcs@outlook.com>
2021-04-18 19:41:22 -0300

    funcionalidades dos botoes

--- src/app/components/file-viewer/file-viewer.component.ts
+++ src/app/components/file-viewer/file-viewer.component.ts
@@ -14,26 +14,46 @@ export class FileViewerComponent implements OnInit {
   @Input() lstArquivos:ListaEncadeada;
   @Input() lstBtn:ListaEncadeada;
 
-  public arq_atual:Arquivo;
-  
+  public arq_atual:{Id:number,Obj:Arquivo};
+  public lst_arquivo: {Id:number,Obj:Arquivo}[] = [];
+  public lst_buttons: {Id:number,Obj:Button}[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
 
-    this.arq_atual = this.lstArquivos.GetConteudoById(0);
+    this.lst_arquivo = this.lstArquivos.GetItensArray();
+    this.lst_buttons = this.lstBtn.GetItensArray();
+
+    this.arq_atual = this.lst_arquivo[0];
 
   }
 
   f_displayProx(){
 
+    console.log("prox");
+    this.arq_atual = this.lstArquivos.GetProxConteudo(this.arq_atual.Obj);
+
   }
 
   f_displayAnt(){
 
+    console.log("Ant");
+    this.arq_atual = this.lstArquivos.GetAntConteudo(this.arq_atual.Obj);
+
   }
 
   f_deleteAtual(){
+
+    console.log("Del");
+    this.arq_atual = this.lstArquivos.RemoveNode(this.arq_atual.Id);
+    this.lst_arquivo = this.lstArquivos.GetItensArray();
+
+  }
+
+  f_redirect(fn){
+
+    this[fn]();
 
   }
 
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -20,16 +20,16 @@ export class HomeComponent implements OnInit {
 
     let arq = []
     arq.push(new Arquivo("file_0","assets/files/other_and_decorative.png","","other_and_decorative.png"));
-    arq.push(new Arquivo("file_1","assets/files/other_and_decorative.png","","other_and_decorative.png"));
+    arq.push(new Arquivo("file_1","assets/files/main_lev_build.png","","main_lev_build.png"));
 
     arq.forEach(element => {
       this.lstArq.AddElement(element);
     });
 
     let buttons = [];
-    buttons.push(new Button("btn_anterior", "", "f_displayAnt();", "", "viewer-prev"))
-    buttons.push(new Button("btn_reset", "", "f_deleteAtual();", "", "viewer-reset"))
-    buttons.push(new Button("btn_proximo", "", "f_displayProx();", "", "viewer-next"))
+    buttons.push(new Button("btn_anterior", "", "f_displayAnt", "", "viewer-prev"))
+    buttons.push(new Button("btn_reset", "", "f_deleteAtual", "", "viewer-reset"))
+    buttons.push(new Button("btn_proximo", "", "f_displayProx", "", "viewer-next"))
 
     buttons.forEach(element => {
       this.lstButton.AddElement(element);
